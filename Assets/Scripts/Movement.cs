89bf62c8c783612292e0f7f6d5e6c3ab80deb2ee
Alpaca-go <32496353@qq.com>
2019-10-27 23:47:31 +0800

    转身&多段跳跃的模块化

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -14,13 +14,15 @@ public class Movement : MonoBehaviour
     public Transform groundCheck, leftCheck, rightCheck;
 
     private float speed = 10;
-    private float jumpForce = 5;
+    private float x, xRaw;
+
+    private float jumpForce = 15;
     private float jumpMax = 2;
     private float jumpHold = 0.1f;
     private float jumpTime;
+
     private float borderSpeed = 2;
     
-
     private bool onGround, onWall;
     private bool isSlide, isClimb;
     private bool isJump;
@@ -47,6 +49,7 @@ public class Movement : MonoBehaviour
 
         Walk();
         Jump();
+        multiJump();
         freeFall();
         siwtchAnim();
     }
@@ -54,23 +57,24 @@ public class Movement : MonoBehaviour
     void FixedUpdate()
     {
         //if (isSlide) wallSlide();
-        if (isClimb) wallGrab();
+        if (isClimb) wallClimb();
     }
 
-    private void Walk()  //地面移动及转身
+    private void Walk()  //地面移动
     {
-        float x = Input.GetAxis("Horizontal");
-        float xRaw = Input.GetAxisRaw("Horizontal");
-
+        x = Input.GetAxis("Horizontal");
         if (x != 0)
         {
             rb.velocity = new Vector2(x * speed, rb.velocity.y);
             anim.SetFloat("walking", Mathf.Abs(xRaw));
         }
-        if (xRaw != 0)
-        {
-            transform.localScale = new Vector3(xRaw, 1, 1);
-        }
+        Flip();
+    }
+
+    private void Flip()  //转身
+    {
+        xRaw = Input.GetAxisRaw("Horizontal");
+        if (xRaw != 0) transform.localScale = new Vector3(xRaw, 1, 1);
     }
 
     /*private void wallSlide()  //贴墙下滑
@@ -81,7 +85,7 @@ public class Movement : MonoBehaviour
         anim.SetBool("grabbing", true);
     }*/
 
-    private void wallGrab()  //爬墙移动
+    private void wallClimb()  //爬墙移动
     {
         float y = Input.GetAxis("Vertical");
         //float yRaw = Input.GetAxisRaw("Vertical");
@@ -93,17 +97,11 @@ public class Movement : MonoBehaviour
             anim.SetBool("falling", false);
             //anim.SetFloat("grabMoving", Mathf.Abs(yRaw));
         }
-        /*if (yRaw != 0)
-        {
-            transform.localScale = new Vector3(1, yRaw, 1);
-        }*/
     }
 
-    private void Jump()  //跳跃
+    /*private void Jump()  //蓄力跳跃
     {
-        //if (onGround) extraJump = 2;  //落地重置多段跳
-
-        if (Input.GetButtonDown("Jump") && onGround && !isJump)
+        if (Input.GetButtonDown("Jump") && !isJump && extraJump > 0)
         {
             isJump = true;
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
@@ -117,43 +115,71 @@ public class Movement : MonoBehaviour
             if (jumpTime < Time.time)
                 isJump = false;
         }
+    }*/
 
-        /*if (Input.GetButtonDown("Jump") && extraJump == 0 && onGround)  //跳完落地
+    /*private void Jump()  //蓄力跳跃+多段跳跃
+    {
+        if (onGround) extraJump = 2;
+        if (Input.GetButtonDown("Jump")  && !isJump && extraJump > 0)
         {
+            isJump = true;
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            jumpTime = Time.time + jumpHold;
+            anim.SetBool("jumping", true);
+        }
+        else if (isJump)
+        {
+            if (Input.GetButton("Jump"))
+                rb.AddForce(new Vector2(0, jumpMax), ForceMode2D.Impulse);
+            if (jumpTime < Time.time)
+            {
+                isJump = false;
+                extraJump--;
+            }
+                
+        }
+    }*/
+
+    private void Jump()  //普通跳跃
+    {
+        if (onGround) isJump = false;
+        if (Input.GetButtonDown("Jump") && onGround && !isJump)  //地面起跳
+        {
+            isJump = true;
             rb.velocity = Vector2.up * jumpForce;
             anim.SetBool("jumping", true);
         }
 
-        if (Input.GetButtonDown("Jump") && extraJump > 0)  //空中多段跳
+        if (Input.GetButtonDown("Jump") && extraJump > 0)  //空中连跳
         {
+            isJump = true;
             rb.velocity = Vector2.up * jumpForce;
-            extraJump--;
             anim.SetBool("jumping", true);
-        }*/
+        }
+    }
+
+    private void multiJump()  //多段跳跃
+    {
+        if (onGround) extraJump = 2;  //连跳次数
+        if (Input.GetButtonDown("Jump") && extraJump > 0) extraJump--;
     }
 
     private void freeFall()  //优化重力
     {
-        float fallDown = 3f;  //重力修正
+        float fallDown = 2f;  //重力修正
         float upResis = 2f;  //上升阻力
 
         if (rb.velocity.y < 0)  //自由落体运动
-        {
             rb.velocity += Vector2.up * Physics2D.gravity.y * fallDown * Time.deltaTime;
-        }
 
         if (rb.velocity.y > 0 && !Input.GetButtonDown("Jump"))   //添加上升阻力，使其不失重
-        {
             rb.velocity += Vector2.up * Physics2D.gravity.y * upResis * Time.deltaTime;
-        }
     }
 
     private void siwtchAnim()  //动画切换
     {
         if (rb.velocity.y < 0 && !onGround && !onWall)  //平抛
-        {
             anim.SetBool("falling", true);
-        }
 
         if (anim.GetBool("jumping"))
         {
@@ -171,8 +197,6 @@ public class Movement : MonoBehaviour
         }
 
         if (onGround)  //落地
-        {
             anim.SetBool("falling", false);
-        }
     }
 }
